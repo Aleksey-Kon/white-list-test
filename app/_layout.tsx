0549d19ae1b7cfc14077497f9b6916192aef3306
Aleksey-Kon <windowsviser@gmail.com>
2026-04-06 23:08:12 +0300

    fix layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,7 @@
 import {
-    DarkTheme,
-    DefaultTheme,
-    ThemeProvider,
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
 } from "@react-navigation/native";
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
@@ -9,17 +9,19 @@ import "react-native-reanimated";
 
 import { useColorScheme } from "@/hooks/use-color-scheme";
 
-export const unstable_settings = {
-  anchor: "(tabs)",
-};
-
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen
+          name="index"
+          options={{
+            headerShown: false,
+            title: "Тест белых списков",
+          }}
+        />
       </Stack>
       <StatusBar style="auto" />
     </ThemeProvider>
